ddaff29b24c7950f768d6042f6e42a1110a3140c
PALASH2201 <palashmahajan22@gmail.com>
2024-09-08 23:44:11 +0530

    updated Cors configuration

--- journalApp/src/main/java/com/edigest/journalApp/config/CorsConfig.java
+++ journalApp/src/main/java/com/edigest/journalApp/config/CorsConfig.java
@@ -9,15 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Bean
-    public WebMvcConfigurer corsConfigurer(){
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://journalize-drab.vercel.app/")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedOrigins("https://journalize-palash2201s-projects.vercel.app/")
-                        .allowedOrigins("https://journalize-git-main-palash2201s-projects.vercel.app/")
+                        .allowedOrigins(
+                                "https://journalize-drab.vercel.app",
+                                "http://localhost:5173",
+                                "https://journalize-palash2201s-projects.vercel.app",
+                                "https://journalize-git-main-palash2201s-projects.vercel.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
